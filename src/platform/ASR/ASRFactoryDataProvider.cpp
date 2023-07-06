e179daeacd25f71929409e7a1c4b9714bb605909
weicheng <xu.wc@outlook.com>
2023-07-06 18:19:02 +0800

    update GetHardwareVersion and GetManufacturingDate

--- src/platform/ASR/ASRFactoryDataProvider.cpp
+++ src/platform/ASR/ASRFactoryDataProvider.cpp
@@ -445,12 +445,33 @@ CHIP_ERROR ASRFactoryDataProvider::GetSerialNumber(char * buf, size_t bufSize)
 
 CHIP_ERROR ASRFactoryDataProvider::GetManufacturingDate(uint16_t & year, uint8_t & month, uint8_t & day)
 {
-    return CHIP_ERROR_NOT_IMPLEMENTED;
+#define OS_YEAR                                                                                                                    \
+    ((((int) (__DATE__[7] - '0') * 10 + (int) (__DATE__[8] - '0')) * 10 + (int) (__DATE__[9] - '0')) * 10 +                        \
+     (int) (__DATE__[10] - '0'))
+
+#define OS_MONTH                                                                                                                   \
+    (__DATE__[2] == 'n' ? (__DATE__[1] == 'a' ? 1 : 6)                                                                             \
+                        : __DATE__[2] == 'b' ? 2                                                                                   \
+                                             : __DATE__[2] == 'r' ? (__DATE__[0] == 'M' ? 3 : 4)                                   \
+                                                                  : __DATE__[2] == 'y' ? 5                                         \
+                                                                                       : __DATE__[2] == 'l'                        \
+                         ? 7                                                                                                       \
+                         : __DATE__[2] == 'g' ? 8                                                                                  \
+                                              : __DATE__[2] == 'p' ? 9 : __DATE__[2] == 't' ? 10 : __DATE__[2] == 'v' ? 11 : 12)
+
+#define OS_DAY ((__DATE__[4] == ' ' ? 0 : __DATE__[4] - '0') * 10 + (__DATE__[5] - '0'))
+
+    year  = (uint16_t) OS_YEAR;
+    month = (uint8_t) OS_MONTH;
+    day   = (uint8_t) OS_DAY;
+
+    return CHIP_NO_ERROR;
 }
 
 CHIP_ERROR ASRFactoryDataProvider::GetHardwareVersion(uint16_t & hardwareVersion)
 {
-    return CHIP_ERROR_NOT_IMPLEMENTED;
+    hardwareVersion = static_cast<uint16_t>(CHIP_DEVICE_CONFIG_DEFAULT_DEVICE_HARDWARE_VERSION);
+    return CHIP_NO_ERROR;
 }
 
 CHIP_ERROR ASRFactoryDataProvider::GetHardwareVersionString(char * buf, size_t bufSize)
